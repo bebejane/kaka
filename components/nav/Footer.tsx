baaf7df363ab0af99e30ac91557e2f6dd5e32c39
Bebe Jane <bjornthief@gmail.com>
2024-06-03 15:54:23 +0200

    clean

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -2,9 +2,7 @@ import s from './Footer.module.scss'
 import cn from 'classnames'
 import type { MenuItem } from '/lib/menu'
 import KFLogo from '/public/images/kf-logo.svg'
-import { useTranslations } from 'next-intl'
 import { usePage } from '/lib/context/page'
-import Logo from '/components/layout/Logo'
 import { PROJECT_NAME } from '/lib/constant'
 
 export type FooterProps = {
@@ -12,14 +10,14 @@ export type FooterProps = {
 	footer: GeneralRecord
 }
 
-export default function Footer({ menu, footer: { email, facebook, instagram, about } }: FooterProps) {
+export default function Footer({ footer: { email, facebook, instagram, about } }: FooterProps) {
 	const { isHome } = usePage()
 
 	return (
 		<footer className={cn(s.footer)} id="footer">
 			<section>
 				<div>
-					Copyright {PROJECT_NAME}, 2023 <br />
+					Copyright {PROJECT_NAME}, 2024 <br />
 					<a href={`mailto:${email}`}>{email}</a>
 				</div>
 				<div>
