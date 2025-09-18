8660046847ad9401dc3dc4e31b748f392be6bb65
Bebe Jane <bjornthief@gmail.com>
2025-09-18 14:53:04 +0200

    reval and web previews for categories

--- pages/api/web-previews.ts
+++ pages/api/web-previews.ts
@@ -1,50 +1,61 @@
 import { withWebPreviewsEdge } from 'dato-nextjs-utils/hoc';
 
 export const config = {
-  runtime: 'edge'
-}
+	runtime: 'edge',
+};
 
 export default withWebPreviewsEdge(async ({ item, itemType }) => {
+	let path = null;
 
-  let path = null;
+	const { api_key } = itemType.attributes;
+	const slug = item.attributes.slug;
 
-  const { api_key } = itemType.attributes
-  const slug = item.attributes.slug
+	switch (api_key) {
+		case 'general':
+			path = `/`;
+			break;
+		case 'start':
+			path = `/`;
+			break;
+		case 'about':
+			path = `/om/${slug}`;
+			break;
+		case 'interview':
+			path = `/intervjuer/${slug}`;
+			break;
+		case 'interview_category':
+			path = `/intervjuer`;
+			break;
+		case 'news':
+			path = `/nyheter/${slug}`;
+			break;
+		case 'youth':
+			path = `/unga/${slug}`;
+			break;
+		case 'youth_category':
+			path = `/unga`;
+			break;
+		case 'recipe':
+			path = `/recept/${slug}`;
+			break;
+		case 'recipe_category':
+			path = `/recept`;
+			break;
+		case 'tip':
+			path = `/tips/${slug}`;
+			break;
+		case 'tip_category':
+			path = `/tips`;
+			break;
+		case 'contact':
+			path = `/kontakt`;
+			break;
+		case 'in_english':
+			path = `/in-english`;
+			break;
+		default:
+			break;
+	}
 
-  switch (api_key) {
-    case 'general':
-      path = `/`
-      break;
-    case 'start':
-      path = `/`
-      break;
-    case 'about':
-      path = `/om/${slug}`
-      break;
-    case 'interview':
-      path = `/intervjuer/${slug}`
-      break;
-    case 'news':
-      path = `/nyheter/${slug}`
-      break;
-    case 'youth':
-      path = `/unga/${slug}`
-      break;
-    case 'recipe':
-      path = `/recept/${slug}`
-      break;
-    case 'tip':
-      path = `/tips/${slug}`
-      break;
-    case 'contact':
-      path = `/kontakt`
-      break;
-    case 'in_english':
-      path = `/in-english`
-      break;
-    default:
-      break;
-  }
-
-  return path
-})
+	return path;
+});
